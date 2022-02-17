888b32384a8399fc5613839ab556c7ccf1d67314
unknown <kravecandrew@gmail.com>
2022-02-17 13:04:15 +0200

    sa

--- src/view/components/Lessons/lesson_21/3.tsx
+++ src/view/components/Lessons/lesson_21/3.tsx
@@ -19,37 +19,37 @@
 
 // const fetch = require('fetch');
 
-function send(url: string) {
-    var options = {
-        method:      'GET',
-        contentType: 'application/json',
-    };
+// function send(url: string) {
+//     var options = {
+//         method:      'GET',
+//         contentType: 'application/json',
+//     };
 
-    return new Promise((resolve, reject) => {
-        fetch(url, options, (error, meta, body) => {
-            if (meta.status === 200) {
-                const { data } = JSON.parse(body);
-                console.log(data);
-                resolve(data);
-            } else {
-                reject(`We have error, status code: ${meta.status}`);
-            }
-        });
-    });
-}
+//     return new Promise((resolve, reject) => {
+//         fetch(url, options, (error, meta, body) => {
+//             if (meta.status === 200) {
+//                 const { data } = JSON.parse(body);
+//                 console.log(data);
+//                 resolve(data);
+//             } else {
+//                 reject(`We have error, status code: ${meta.status}`);
+//             }
+//         });
+//     });
+// }
 
 
-//После рефакторинга
+// //После рефакторинга
 
-const url = 'https://my-json-server.typicode.com/AndrewKrava/json-server';
+// const url = 'https://my-json-server.typicode.com/AndrewKrava/json-server';
 
-send(url)
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+// send(url)
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
 
 export const taskNumber = 3;
 
